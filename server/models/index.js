a64e87614e39e35f1db44fab7192f40a44f17517
mitchdmarino <mitchdmarino@gmail.com>
2022-08-08 14:33:30 -0700

    trips-start

--- server/models/index.js
+++ server/models/index.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/mernAuth'
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1/trips";
 
-mongoose.connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI);
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.once('open', () => {
-    console.log(`connected to MongoDB @ ${db.host}: ${db.port}`)
-})
+db.once("open", () => {
+  console.log(`connected to MongoDB @ ${db.host}: ${db.port}`);
+});
 
-db.on('error', err => {
-    console.error('database is not very happy 😓', err)
-})
+db.on("error", (err) => {
+  console.error("database is not very happy 😓", err);
+});
 
 module.exports = {
-    // export db models 
-    User: require('./user')
-}
+  // export db models
+  User: require("./user"),
+};
